5bfcb1f7d23224ebfa1d4f8aaa026f402a1c51c6
kiu_sharangia.anastasia <sharangia.anastasia@kiu.edu.ge>
2025-04-26 18:25:33 +0400

    started the web server

--- webServer/Program.cs
+++ webServer/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System.Net;
+using System.Net.Sockets;
+
+//1. Start the Web Server
+//created TCP server on port 8080 
+var listener = new TcpListener(IPAddress.Any, 8080);
+listener.Start();
+
+//accepting incoming client connections 
+var client = listener.AcceptTcpClient();
+var stream = client.GetStream();
+
